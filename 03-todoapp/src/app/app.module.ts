e57cd27a3c27dc5ba97f3987162d0c4fa3926a84
Jesus Peralta <jesusperaltac@gmail.com>
2018-12-09 20:34:13 +0100

    added redux devtools

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { TodoComponent } from './todo/todo.component';
-import { TodosListComponent } from './todo/todos-list/todos-list.component';
-import { TodoItemComponent } from './todo/todo-item/todo-item.component';
-import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
-import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {TodoComponent} from './todo/todo.component';
+import {TodosListComponent} from './todo/todos-list/todos-list.component';
+import {TodoItemComponent} from './todo/todo-item/todo-item.component';
+import {TodoFooterComponent} from './todo/todo-footer/todo-footer.component';
+import {TodoAddComponent} from './todo/todo-add/todo-add.component';
+import {StoreModule} from '@ngrx/store';
+import {todoReducer} from './todo/todo.reducer';
+import {environment} from '../environments/environment';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -20,9 +24,15 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
     TodoAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({todos: todoReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
