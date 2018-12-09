ec31fa994aaffc59de865738c4cef1100facd752
Jesus Peralta <jesusperaltac@gmail.com>
2018-12-09 21:02:21 +0100

    added store to todo list component

--- 03-todoapp/src/app/todo/todo.reducer.ts
+++ 03-todoapp/src/app/todo/todo.reducer.ts
@@ -1,7 +1,10 @@
 import {actions, ADD_TODO} from './todo.actions';
 import {Todo} from './model/todo';
 
-const initialState: Todo[] = [];
+const todo1 = new Todo('Clean the kitchen');
+const todo2 = new Todo('Read the newspaper');
+
+const initialState: Todo[] = [todo1, todo2];
 
 export function todoReducer(state = initialState, action: actions): Todo[] {
 
