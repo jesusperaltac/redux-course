4f953a974345ec28962b05a092ab00707fe39f06
Jesus Peralta <jesusperaltac@gmail.com>
2018-12-09 20:52:50 +0100

    added form to todo add component

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import {StoreModule} from '@ngrx/store';
 import {todoReducer} from './todo/todo.reducer';
 import {environment} from '../environments/environment';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     StoreModule.forRoot({todos: todoReducer}),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
